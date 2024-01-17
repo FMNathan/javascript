d8e30041adbe9f123ffeda686b6c093be981ac5c
Nathan de Farias Melo <111164902+FMNathan@users.noreply.github.com>
2024-01-17 00:13:51 -0300

    Praticando um pouco de JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} Kmph`)
+if(vel > 60){  //Condição Simples
+    console.log(`Você ultrapasou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre utilizando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,20 @@
+/* Condição Simples 
+if(condição){
+    true
+}
+
+Condição Composta 
+if(condição){
+    true
+} else{
+    false
+}
+*/
+
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é Estrangeiro')
+}
