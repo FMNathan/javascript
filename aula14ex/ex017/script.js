a53c6698d6115531b8ea1cc2b65f534856127bd5
Nathan de Farias Melo <111164902+FMNathan@users.noreply.github.com>
2024-01-25 18:37:42 -0300

    Criando Tabuada com JavaScript

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Olá')
+function Tabuada(){
+    let num = document.getElementById('txtn')
+    let tabuada = document.getElementById('selTab')
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    }else{
+        let n = Number(num.value)
+        let c = 1 
+        tabuada.innerHTML = ''
+        while (c  <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    }
+}
